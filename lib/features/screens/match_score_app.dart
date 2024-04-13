2b68f263f515d2213c4446d027f1644eea1f46fe
nameisjayant <jayntkumar99@gmail.com>
2024-04-13 20:14:37 +0530

    feat(ui): added selection section of live match app ui

--- lib/features/screens/match_score_app.dart
+++ lib/features/screens/match_score_app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:one_screen_flutter/components/icon_component.dart';
+import 'package:one_screen_flutter/components/spacer_component.dart';
 import 'package:one_screen_flutter/ui/colors.dart';
 import 'package:one_screen_flutter/utils/image_path.dart';
 
@@ -8,7 +8,7 @@ class MatchScoreApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         body: MatchScoreScreen(),
         backgroundColor: AppColors.matchBg,
@@ -17,18 +17,40 @@ class MatchScoreApp extends StatelessWidget {
   }
 }
 
-class MatchScoreScreen extends StatelessWidget {
-  MatchScoreScreen({super.key});
+class MatchScoreScreen extends StatefulWidget {
+  const MatchScoreScreen({super.key});
 
+  @override
+  State<MatchScoreScreen> createState() => _MatchScoreScreenState();
+}
+
+class _MatchScoreScreenState extends State<MatchScoreScreen> {
   final TextEditingController search = TextEditingController();
+  final List<SportModal> sportModalList = [
+    SportModal(id: 1, title: 'Soccer', icon: ImagePath.soccer),
+    SportModal(id: 2, title: 'Basket Ball', icon: ImagePath.basketball),
+    SportModal(id: 3, title: 'Rubgy', icon: ImagePath.rubgy),
+    SportModal(id: 4, title: 'Base Ball', icon: ImagePath.baseball),
+    SportModal(id: 5, title: 'Cricket', icon: ImagePath.cricket)
+  ];
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: [_searchTextField(controller: search)],
+      children: [
+        _searchTextField(controller: search),
+        spacerHeight(height: 20),
+        sportSection(
+            selectedIndex: selectedIndex,
+            onValueUpdate: (val) {
+              setState(() {
+                selectedIndex = val;
+              });
+            })
+      ],
     );
   }
-
 
   // search text field
   Widget _searchTextField({required TextEditingController controller}) {
@@ -41,17 +63,109 @@ class MatchScoreScreen extends StatelessWidget {
               borderRadius: BorderRadius.circular(14)),
           child: TextField(
             controller: controller,
-            decoration: InputDecoration(
-                hintText: 'Search your competition',
-                hintStyle: const TextStyle(
+            decoration: const InputDecoration(
+                hintText: 'Search your competition...',
+                hintStyle: TextStyle(
                     color: AppColors.matchDarkGray,
                     fontSize: 14,
                     fontFamily: 'SourceSansPro',
                     fontWeight: FontWeight.normal),
-                prefixIcon: svgIconComponent(icon: ImagePath.searchIcon),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: AppColors.matchDarkGray,
+                  size: 30,
+                ),
                 border: InputBorder.none),
             style: const TextStyle(color: Colors.white),
           ),
         ));
   }
+
+  // sport section
+  Widget sportSection(
+      {required int selectedIndex, required Function(int) onValueUpdate}) {
+    return SizedBox(
+      height: 50,
+      child: ListView.builder(
+        itemBuilder: (BuildContext context, index) {
+          return selectionRow(
+              title: sportModalList[index].title,
+              index: index,
+              icon: sportModalList[index].icon,
+              selected: selectedIndex == index,
+              onValueUpdate: onValueUpdate);
+        },
+        itemCount: sportModalList.length,
+        scrollDirection: Axis.horizontal,
+      ),
+    );
+  }
+
+
+  // sport selection Row
+  Widget selectionRow(
+      {required String title,
+      required int index,
+      required String icon,
+      required bool selected,
+      required Function(int) onValueUpdate}) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 8.0,left: 16),
+      child: InkWell(
+        onTap: () {
+          onValueUpdate(index);
+        },
+        highlightColor: Colors.transparent,
+        splashColor: Colors.transparent,
+        child: Container(
+          decoration: BoxDecoration(
+              color: selected
+                  ? AppColors.matchTextField
+                  : AppColors.matchTextField,
+              gradient: selected
+                  ? const LinearGradient(colors: [
+                      AppColors.matchLightOrange,
+                      AppColors.matchOrange
+                    ])
+                  : const LinearGradient(colors: [
+                      AppColors.matchTextField,
+                      AppColors.matchTextField
+                    ]),
+              borderRadius: BorderRadius.circular(50)),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              children: [
+                Image.asset(
+                  icon,
+                  width: 24,
+                  height: 24,
+                ),
+                if (selected)
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Text(
+                      title,
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w900,
+                          fontFamily: 'SourceSansPro'),
+                    ),
+                  )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SportModal {
+  int id;
+  String title;
+  String icon;
+
+  SportModal({required this.id, required this.title, required this.icon});
 }
--- lib/ui/colors.dart
+++ lib/ui/colors.dart
@@ -7,4 +7,6 @@ class AppColors {
   static const Color matchBg = Color(0XFF181829);
   static const Color matchDarkGray = Color(0XFF65656B);
   static const Color matchTextField = Color(0XFF222232);
+  static const Color matchLightOrange = Color(0XFFF4A58A);
+  static const Color matchOrange = Color(0XFFED6B4E);
 }
